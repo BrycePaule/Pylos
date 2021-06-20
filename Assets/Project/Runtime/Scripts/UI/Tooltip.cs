ef70f27d7882e6c82083d29895a0c623a0596097
BrycePaule <egymlol@gmail.com>
2021-06-20 18:19:05 +1000

    - "Fixed" NPC tooltips - everything is super manual, but works for the current time

--- Assets/Project/Runtime/Scripts/UI/Tooltip.cs
+++ Assets/Project/Runtime/Scripts/UI/Tooltip.cs
@@ -82,55 +82,97 @@ public class Tooltip : MonoBehaviour
 		UpdateHeader(obj);
 		UpdateHealthBar(obj);
 
-		// if (obj.GetComponent<Item>())
-		// {
-		// 	Item item = obj.GetComponent<Item>();
-		// 	BuildDisplayProperties<Item>(obj, item);
-		// 	UpdateInfoBoxSize<Item>(obj);
-		// 	return;
-		// }
-
 		if (obj.GetComponent<NPCBase>())
 		{
 			NPCBase npcBase = obj.GetComponent<NPCBase>();
-			BuildDisplayProperties<NPCBase>(obj, npcBase);
-			UpdateInfoBoxSize<NPCBase>(obj);
+			BuildDisplayProperties(npcBase);
 			return;
 		}
+		// else if (obj.GetComponent<Container>())
+		// {
+		// 	Container container = obj.GetComponent<Container>();
+		// 	BuildDisplayProperties(container);
+		// 	return;
+		// }
 
 		DisableAllProperties();
 
 	}
 
-	private void UpdateInfoBoxSize<T>(GameObject obj)
+	private void UpdateInfoBoxSize(int fieldCount)
 	{
-		tooltipRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, tooltipBaseHeight + ((HealthBar.activeInHierarchy ? 0 : 1) * -10) + (typeof(T).GetFields().Length * propHeight));
+		tooltipRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, tooltipBaseHeight + ((HealthBar.activeInHierarchy ? 0 : 1) * -10) + (fieldCount * propHeight));
 	}
 
-	private void BuildDisplayProperties<T>(GameObject obj, T comp)
+	//	HEADER
+	private void UpdateHeader(GameObject obj)
 	{
-		int fieldCount = typeof(T).GetFields().Length;
+		HeaderText.text = obj.name;
+		SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
+		Icon.sprite = sr.sprite;
+		Icon.color = sr.color;
+	}
 
-		// create missing property elements (if needed)
-		int missingProps = fieldCount - propPool.Count;
-		for (int i = 0; i < missingProps; i++)
+	// PROPERTY ELEMENTS
+	private void BuildDisplayProperties(NPCBase npc)
+	{
+		FieldInfo[] fields = new FieldInfo[6];
+		fields[0] = typeof(Movement).GetField("TileLoc");
+		fields[1] = typeof(NPCBase).GetField("Faction");
+		fields[2] = typeof(Movement).GetField("MoveDelay");
+		fields[3] = typeof(Combat).GetField("Damage");
+		fields[4] = typeof(Combat).GetField("AttackRange");
+		fields[5] = typeof(Combat).GetField("AttackSpeed");
+
+		CreateMissingProperties(fields.Length);
+
+		SetPropertyElement<Movement>(propPool[0], fields[0], (Movement) npc.GetNPCComponent(NPCComponentType.Movement));
+		SetPropertyElement<NPCBase>(propPool[1], fields[1], npc);
+		SetPropertyElement<Movement>(propPool[2], fields[2], (Movement) npc.GetNPCComponent(NPCComponentType.Movement));
+		SetPropertyElement<Combat>(propPool[3], fields[3], (Combat) npc.GetNPCComponent(NPCComponentType.Combat));
+		SetPropertyElement<Combat>(propPool[4], fields[4], (Combat) npc.GetNPCComponent(NPCComponentType.Combat));
+		SetPropertyElement<Combat>(propPool[5], fields[5], (Combat) npc.GetNPCComponent(NPCComponentType.Combat));
+
+		DisableUnusedProperties(fields.Length);
+		UpdateInfoBoxSize(fields.Length);
+	}
+
+	private void BuildDisplayProperties(Container container)
+	{
+		print(container.items.Count);
+		FieldInfo[] fields = new FieldInfo[container.items.Count + 1];
+		fields[0] = typeof(Container).GetField("TileLoc");
+		fields[1] = typeof(Dictionary<ItemID, int>).GetField("Key");
+
+		CreateMissingProperties(fields.Length);
+
+		SetPropertyElement<Container>(propPool[0], fields[0], container);
+		SetPropertyElement<Dictionary<ItemID, int>>(propPool[1], fields[1], container.items);
+
+		DisableUnusedProperties(fields.Length);
+		UpdateInfoBoxSize(fields.Length);
+	}
+
+	private void SetPropertyElement<T>(PropertyElement prop, FieldInfo field, T obj)
+	{
+		prop.propertyText = field.Name;
+		prop.valueText = field.GetValue(obj).ToString();
+		prop.gameObject.SetActive(true);
+	}
+
+	private void CreateMissingProperties(int fieldCount)
+	{
+		for (int i = 0; i < fieldCount - propPool.Count; i++)
 		{
 			propPool.Add(Instantiate(PropertyElementPrefab, new Vector3(0, 0, 0), Quaternion.identity, InfoBox.transform).GetComponent<PropertyElement>());
 		}
+	}
 
-		// disable unused property elements
+	private void DisableUnusedProperties(int fieldCount)
+	{
 		for (int i = fieldCount; i < propPool.Count; i++)
 		{
 			propPool[i].gameObject.SetActive(false);
-		}
-
-		// update and enable relevant fields
-		FieldInfo[] fields = typeof(T).GetFields();
-		for (int i = 0; i < fieldCount; i++)
-		{
-			propPool[i].propertyText = fields[i].Name;
-			propPool[i].valueText = fields[i].GetValue(comp).ToString();
-			propPool[i].gameObject.SetActive(true);
 		}
 	}
 
@@ -144,6 +186,7 @@ public class Tooltip : MonoBehaviour
 		tooltipRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, tooltipBaseHeight + ((HealthBar.activeInHierarchy ? 0 : 1) * -10));
 	}
 
+	// HEALTHBAR
 	private void UpdateHealthBar(GameObject obj)
 	{
 		if (obj.GetComponentInChildren<Health>() != null)
@@ -160,13 +203,6 @@ public class Tooltip : MonoBehaviour
 		}
 	}
 
-	private void UpdateHeader(GameObject obj)
-	{
-		HeaderText.text = obj.name;
-		SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
-		Icon.sprite = sr.sprite;
-		Icon.color = sr.color;
-	}
 
 	public void EnableTooltip() 
 	{ 
